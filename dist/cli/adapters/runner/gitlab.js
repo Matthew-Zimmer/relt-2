7423743407d537fb8ec9d86091d28edab9425e7c
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2023-03-05 02:41:06 +0000

    Initial Commit

--- dist/cli/adapters/runner/gitlab.js
+++ dist/cli/adapters/runner/gitlab.js
@@ -16,45 +16,45 @@ function gitlabRunnerAdapter(project) {
     return {
         createConfigs: () => __awaiter(this, void 0, void 0, function* () {
             yield (0, promises_1.writeFile)(`${name}/.gittlab-ci.yml`, `\
-  stages:
-    - deploy
-  
-  image: relt:latest
-  
-  deploy preview:
-    stage: deploy
-    rules:
-      - if: '$CI_PIPELINE_SOURCE == "merge_request_submit"'
-    script: |
-      relt deploy $branch --using aws databricks webex
-  
-  redeploy preview:
-    stage: deploy
-    rules:
-      - if: '$CI_COMMIT_REF_NAME != "master" && $CI_PIPELINE_SOURCE == "push"'
-    script: |
-      relt redeploy $branch --using aws databricks webex
-  
-  destroy preview:
-    stage: deploy
-    rules:
-      - if: '$CI_PIPELINE_SOURCE == "merge_request_closed"'
-    script: |
-      relt destroy $branch --using aws databricks webex
-  
-  deploy prod check:
-    stage: deploy
-    rules:
-      - if: '$CI_COMMIT_REF_NAME == "master" && $CI_PIPELINE_SOURCE == "push"'
-    script: |
-      relt redeploy $branch --using aws databricks webex
-  
-  deploy prod:
-    stage: deploy
-    rules:
-      - if: '$CI_COMMIT_REF_NAME == "master" && $CI_PIPELINE_SOURCE == "tag"'
-    script: |
-      relt redeploy $branch --using aws databricks webex
+stages:
+  - deploy
+
+image: relt:latest
+
+deploy preview:
+  stage: deploy
+  rules:
+    - if: '$CI_PIPELINE_SOURCE == "merge_request_submit"'
+  script: |
+    relt deploy $branch --using aws databricks webex
+
+redeploy preview:
+  stage: deploy
+  rules:
+    - if: '$CI_COMMIT_REF_NAME != "master" && $CI_PIPELINE_SOURCE == "push"'
+  script: |
+    relt redeploy $branch --using aws databricks webex
+
+destroy preview:
+  stage: deploy
+  rules:
+    - if: '$CI_PIPELINE_SOURCE == "merge_request_closed"'
+  script: |
+    relt destroy $branch --using aws databricks webex
+
+deploy prod check:
+  stage: deploy
+  rules:
+    - if: '$CI_COMMIT_REF_NAME == "master" && $CI_PIPELINE_SOURCE == "push"'
+  script: |
+    relt redeploy $branch --using aws databricks webex
+
+deploy prod:
+  stage: deploy
+  rules:
+    - if: '$CI_COMMIT_REF_NAME == "master" && $CI_PIPELINE_SOURCE == "tag"'
+  script: |
+    relt redeploy $branch --using aws databricks webex
 `);
         })
     };
